0f90526d1d86893aa5c17989346b28bbba6da4af
jimishol <hol.jimis@gmail.com>
2021-09-16 21:56:45 +0300

    Add files via upload
    Keeps basic constants of economy calibration.

---
+++ docs/economy_mod/code/libs/Calibration.lua
@@ -0,0 +1,12 @@
+-- Copyright © 2008-2021 Pioneer Developers. See AUTHORS.txt for details
+-- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
+
+local Calibration = {}
+
+Calibration.StockPriceOne = 10^9 --On low prices rn are large integers that can not exceed much the 2    *10^9 anyway. See function updateEquipmentStock in SpaceStation.lua
+Calibration.PriceExponential = 2.42328282947206
+
+print("----------------------------------")
+print(Calibration.StockPriceOne, Calibration.PriceExponential)
+print("--------StockOne, PriceExp--------")
+return Calibration 
